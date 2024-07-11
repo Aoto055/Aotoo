24051ed44cecbb34d2c43d71dbff36f25b7519b6
Aoto055 <salderyanchiu@gmail.com>
2024-07-11 23:10:01 +0800

    index.js

--- index.js
+++ index.js
@@ -30,12 +30,12 @@ client.on('ready', async () => {
     .setName('')
     .setDetails(`Valorant [${formatTime()}]`)
     .setStartTimestamp(Date.now())
- .setAssetsLargeImage('') //You can put links in tenor or discord and etc.
+ .setAssetsLargeImage('https://media.discordapp.net/attachments/1138857476847054941/1260975816305541182/e2b2500cbd210f64d451c9bdd773060e.png?ex=669146be&is=668ff53e&hm=4a89a495fbc797813ded2b409ece0e159964db7669d2af72e07848c068f6d6c6&=&format=webp&quality=lossless&width=350&height=350') //You can put links in tenor or discord and etc.
     .setAssetsLargeText('Boring') //Text when you hover the Large image
-    .setAssetsSmallImage('') //You can put links in tenor or discord and etc.
+    .setAssetsSmallImage('https://cdn.discordapp.com/emojis/1167746213307756566.gif?size=96&quality=lossless') //You can put links in tenor or discord and etc.
     .setAssetsSmallText('Twitch') //Text when you hover the Small image
-    .addButton('Watch', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ')
-    .addButton('Donate', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+    .addButton('Watch', 'https://www.youtube.com/watch?v=PymbRTMb4hY')
+    .addButton('wya', 'https://www.youtube.com/watch?v=PymbRTMb4hY');
 
   client.user.setActivity(r);
   client.user.setPresence({ status: "dnd" }); //dnd, online, idle, offline
@@ -44,7 +44,7 @@ client.on('ready', async () => {
   setInterval(() => {
     const newTime = formatTime();
     if (newTime !== prevTime) {
-      const newDetails = ` [${newTime}]`;
+      const newDetails = `ako may ari ng Eporium`;
       r.setDetails(newDetails);
       client.user.setActivity(r);
       prevTime = newTime;
